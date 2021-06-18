152dab5409a7584910f5a38c91b7a9f31d5ac03d
Ammbika <sharad.srivastava1@gmail.com>
2021-06-18 14:07:23 +0530

    Creating my own kafka connector using arch type from https://github.com/jcustenborder/kafka-connect-archtype

---
+++ src/test/java/com/sharad/kafka/connector/DocumentationTest.java
@@ -0,0 +1,7 @@
+package com.sharad.kafka.connector;
+
+import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
+
+public class DocumentationTest extends BaseDocumentationTest {
+
+}
